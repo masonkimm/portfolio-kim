27713928b21330afbd42a51b56b5681fd5cdbb6b
SungJin Kim <sungjinkimm@gmail.com>
2021-04-14 12:26:26 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <img src={logo} className="app-logo" alt="logo" />
+
+      <h1>Mason SungJin Kim</h1>
+      <h2>Full Stack Developer</h2>
+    </div>
+  );
+}
+
+export default App;
